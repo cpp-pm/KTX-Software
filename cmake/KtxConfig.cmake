0ac3f4effca64622d1fba364c09b41028f86cf04
Rahul Sheth <rahul@snap.com>
2021-07-07 14:46:52 -0400

    Use our own config file

--- cmake/KtxConfig.cmake
+++ cmake/KtxConfig.cmake
@@ -4,10 +4,4 @@
 # include(CMakeFindDependencyMacro)
 # find_dependency()
 
-find_package(basis_universal CONFIG REQUIRED)
-find_package(OpenGL-Registry CONFIG REQUIRED)
-if(@KTX_FEATURE_VULKAN@)
-    find_package(dfdutils CONFIG REQUIRED)
-endif()
-
 include("${CMAKE_CURRENT_LIST_DIR}/KtxTargets.cmake")
